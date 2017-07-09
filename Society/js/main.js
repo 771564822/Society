929375862e053e51e92362ad933f72a4626f4890
王朋 <771564822@qq.com>
2017-07-09 17:27:41 +0800

    网站项目

---
+++ Society/js/main.js
@@ -0,0 +1,22 @@
+function setTab(name, cursel) {
+	cursel_0 = cursel;
+	for(var i = 1; i <= links_len; i++) {
+		var menu = document.getElementById(name + i);
+		var menudiv = document.getElementById("con_" + name + "_" + i);
+		if(i == cursel) {
+			menu.className = "off";
+			menudiv.style.display = "block";
+		} else {
+			menu.className = "";
+			menudiv.style.display = "none";
+		}
+	}
+}
+var name_0 = 'one';
+var cursel_0 = 1;
+var links_len, iIntervalId;
+onload = function() {
+var links = document.getElementById("tab1").getElementsByTagName('li')
+	links_len = links.length;
+	setTab(name_0, cursel_0);
+}
